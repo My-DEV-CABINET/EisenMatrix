3fd110a72f2993192e0562d666413ba3aa5fc929
jakkujakku <jakkujakku98@gmail.com>
2024-04-09 00:28:54 +0900

    init: 저장소 생성

---
+++ EisenMatrix/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EisenMatrix
+//
+//  Created by 준우의 MacBook 16 on 4/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EisenMatrix/EisenMatrixApp.swift
@@ -0,0 +1,17 @@
+//
+//  EisenMatrixApp.swift
+//  EisenMatrix
+//
+//  Created by 준우의 MacBook 16 on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct EisenMatrixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
